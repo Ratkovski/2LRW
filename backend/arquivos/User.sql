0d486e612c56fce1574b11262bb10c3f4c107d93
Weslley Almeida <weslley_fac@hotmail.com>
2020-11-14 11:32:47 -0300

    alter Makefile and added arquivos

---
+++ backend/arquivos/User.sql
@@ -0,0 +1,10 @@
+insert into User (name, balance) values ('Ivory', 74575.75);
+insert into User (name, balance) values ('Lavinie', 22461.59);
+insert into User (name, balance) values ('Josy', 20760.22);
+insert into User (name, balance) values ('Reine', 99869.77);
+insert into User (name, balance) values ('Yoshiko', 12752.73);
+insert into User (name, balance) values ('Sergeant', 86587.43);
+insert into User (name, balance) values ('Diandra', 52501.69);
+insert into User (name, balance) values ('Belvia', 39926.2);
+insert into User (name, balance) values ('Chloette', 85040.45);
+insert into User (name, balance) values ('Jsandye', 63465.46);
