470e0aff856d85a8e639c924fc87278004bc8245
Weslley Almeida <weslley_fac@hotmail.com>
2020-11-15 10:45:40 -0300

    change to json

--- backend/shawee/ext/site/main.py
+++ backend/shawee/ext/site/main.py
@@ -9,28 +9,17 @@ from datetime import datetime, date, time
 bp = Blueprint("site", __name__)
 
 
-@bp.route('/', methods=['GET', 'POST'])
-def index():
-    return 'Deu certo aqui'
-
-
 @bp.route('/user', methods=['POST'])
 def user():
-    id = int(request.form['id'])
-    user = User.query.filter_by(id=id).first()
+    """
+        Recebe id e retorna as espeficações do usuario 
+    """
+    data = request.get_json()
+    user_id = data.get('id')
+    user = User.query.filter_by(id=user_id).first()
 
-    result = {
-        'id': id,
-        'name': user.name
-    }
-    return jsonify(result)
+    return jsonify(user.to_dict())
 
-
-""" 
-data inicial e final
-caixa atual
-total de movimentações
-"""
 
 
 @bp.route('/transactions', methods=['POST'])
@@ -38,8 +27,9 @@ def transactions():
     """ 
         Returna todas as transações de um usuario
     """
-    id = int(request.form['id'])
-    transaction = Transactions.query.filter_by(user_id=id).all()
+    data = request.get_json()
+    user_id = data.get('id')
+    transaction = Transactions.query.filter_by(user_id=user_id).all()
 
     result = []
 
@@ -53,10 +43,9 @@ def transactions():
 @bp.route('/category', methods=['POST'])
 def category():
     """
-        Retorna transações por categoria e data
+        Retorna transações por categoria e um intervalo de tempo
     """
     data = request.get_json()
-    import ipdb; ipdb.set_trace()
     user_id = data.get('id')
     category = data.get('category')
     datein = data.get('datein')
@@ -71,6 +60,8 @@ def category():
 
     return jsonify(result)
 
+
+
 @bp.route('/users', methods=['GET'])
 def idex_users():
     users = User.query.all()
@@ -80,6 +71,7 @@ def idex_users():
     for user in users:
         user_list.append(user.to_dict())
     return jsonify(user_list)
+
 
 
 @bp.route('/transactions-by-category', methods=['POST'])
@@ -105,9 +97,12 @@ def transacitons_categorie():
     return jsonify(transactions_category)
 
 
+
 @bp.route('/total_value_month', methods=['POST'])
 def total_value():
-
+    """
+        Recebe id, data inicio e data final e retorna total de por mês e ano 
+    """
     data = request.get_json()
     user_id = data.get('id')
     datein = data.get('datein')
