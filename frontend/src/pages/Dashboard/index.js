559b4a25710344bc547a91a13ee2ac439fa21939
Lucas Rocha <lucas_rocha.14@outlook.com>
2020-11-15 17:44:34 -0300

    corrections

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,12 +1,12 @@
-import React, {useRef, useCallback, useState} from 'react';
+import React, {useRef, useCallback, useState, useEffect} from 'react';
 import { useSelector } from 'react-redux';
-import { Bar } from 'react-chartjs-2';
+import { Bar, Pie } from 'react-chartjs-2';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
-import history from '~/services/history';
+import api from '~/services/api';
 
-import { Container, Content } from './styles';
+import { Container, Content, Select } from './styles';
 
 import Nav from '~/components/Nav';
 import Input from '~/components/Input';
@@ -17,10 +17,24 @@ export default function Dashboard() {
   const [labels, setLabels] = useState([]);
   const [expenses, setExpenses] = useState([]);
   const [profits, setProfits] = useState([]);
+  const [onSelect, setOnSelect] = useState(false);
+  const [categories, setCategories] = useState([]);
   const nav = useSelector(state => state.navDashboard.dashboard);
 
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const response = await api.get('/categories');
+
+      console.log(response.data);
+      setCategories({...categories,...response.data});
+      console.log('testeeee '+ categories);
+    }
+
+    fetchCategories();
+  },[])
+
   const handleSubmit = useCallback(async (data) => {
-    try {
+    // try {
       formRef.current.setErrors({});
       const schema = Yup.object().shape({
         initialDate: Yup.date()
@@ -28,73 +42,171 @@ export default function Dashboard() {
         finalDate: Yup.date().required('Data final obrigatória'),
       });
 
-      // await schema.validate(data, {
-      //   abortEarly: false,
-      // });
 
-      // history.push('/dashboard');
+      const categoriesCheckbox = document.querySelectorAll("input[type='checkbox']");
+      // let checkboxs = '';
+      let checkboxs = [];
+      for (let i = 0; i < categoriesCheckbox.length; i++) {
+        if (categoriesCheckbox[i].checked) {
+          // checkboxs += categoriesCheckbox[i].defaultValue + ',';
+          checkboxs.push(categoriesCheckbox[i].defaultValue);
+        }
+      }
 
-      const values = [
-          {
-              value: 146375.13,
-              date:'01-2020',
-              status: 1
-          },
-          {
-              value: 202755.91999999998,
-              date:'01-2020',
-              status: 2
-          },
-          {
-              value:94068.39,
-              date: '02-2020',
-              status: 1
-          },
-          {
-              value:94068.39,
-              date: '03-2020',
-              status: 2
-          },
-      ];
+      if(checkboxs.length === 0) {
+        alert('Selecione alguma categoria');
+        return;
+      }
+      // checkboxs = checkboxs.substr(0,1);
+
+      console.log(checkboxs)
+
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      const {initialDate, finalDate} = data;
+
+
+      if(new Date(initialDate) > new Date(finalDate)) {
+        alert('Data inicial não pode ser maior que data final');
+        return;
+      }
+
+      const response = await api.post('/total_value_month', {
+        id: 1,
+        datein: initialDate,
+        dateout: finalDate
+      });
+
+      // const values = [
+      //     {
+      //         value: 146375.13,
+      //         date:'01-2020',
+      //            category: 1,
+      //         status: 1
+      //     },
+      //     {
+      //         value: 202755.91999999998,
+      //         date:'01-2020',
+      //         status: 2
+      //     },
+      //     {
+      //         value:94068.39,
+      //         date: '02-2020',
+      //         status: 1
+      //     },
+      //     {
+      //         value:94068.39,
+      //         date: '03-2020',
+      //         status: 2
+      //     },
+      // ];
 
       const labels = [];
       const expense = [];
       const profit = [];
 
+      // response.data.forEach(index => {
+      //   const existDate = labels.find(element => element === index.date);
+
+      //   if(!existDate) {
+      //     labels.push(index.date);
+      //   }
+
+      //   const amountDate = response.data.filter(element => element.date === index.date);
+
+      //   if(index.status === 1) {
+      //     profit.push(index.value);
+      //   } else if(index.status === 2) {
+      //     expense.push(index.value);
+      //   }
+
+      //   if(amountDate.length === 1) {
+      //     if(index.status === 1) {
+      //       expense.push(0);
+      //     } else if(index.status === 2) {
+      //       profit.push(0);
+      //     }
+      //   }
+      // });
+
+      const values = [
+        {
+          "category": 1,
+          "date": "01-2020",
+          "status": 1,
+          "value": 77362.71
+        },
+        {
+          "category": 4,
+          "date": "01-2020",
+          "status": 1,
+          "value": 69012.42
+        },
+        {
+          "category": 2,
+          "date": "01-2020",
+          "status": 2,
+          "value": 39081.14
+        },
+        {
+          "category": 3,
+          "date": "01-2020",
+          "status": 2,
+          "value": 85659.73
+        },
+      ];
+
+      let labels2 = [];
+
       values.forEach(index => {
-        const existDate = labels.find(element => element == index.date);
+        const existDate = labels.find(element => element === index.date);
+
+        console.log(categories);
+        const findNameCategory = categories.find(element => element.id === index.category);
+
+        console.log(findNameCategory + 'rer');
+        const existCategory = labels2.find(element => element === findNameCategory.name);
+
+
+        if(!existCategory) {
+
+          labels2.push(findNameCategory);
+          console.log(labels2);
+        }
 
         if(!existDate) {
           labels.push(index.date);
         }
 
-        const amountDate = values.filter(element => element.date == index.date);
+        const amountDate = values.filter(element => element.date === index.date);
 
-        if(index.status == 1) {
+        if(index.status === 1) {
           profit.push(index.value);
-        } else if(index.status == 2) {
-          expense.push(index.value);
         }
 
-        if(amountDate.length == 1) {
-          if(index.status == 1) {
-            expense.push(0);
-          } else if(index.status == 2) {
-            profit.push(0);
-          }
-        }
+        // if(amountDate.length === 1) {
+        //   if(index.status === 1) {
+        //     expense.push(0);
+        //   } else if(index.status === 2) {
+        //     profit.push(0);
+        //   }
+        // }
       });
+
 
       setLabels(labels);
       setExpenses(expense);
       setProfits(profit);
-    } catch (err) {
-        formRef.current.setErrors(err);
+    // } catch (err) {
+    //     formRef.current.setErrors(err);
 
-        alert('Preencha todos os dados!');
+    //     alert('Preencha todos os dados!');
 
-        return;
-    }
+    //     return;
+    // }
   },[]);
 
 
@@ -122,12 +234,46 @@ export default function Dashboard() {
     }
   }
 
+  const options = {
+    scales: {
+      xAxes: [{
+        stacked: true
+      }],
+      yAxes: [{
+        stacked: true
+      }]
+    }
+  }
+
+
+const data2 = {
+labels: labels,
+  datasets: [
+        {
+          label: 'Comida',
+          backgroundColor: "#caf270",
+          data: [12, 59, 5, 56, 58,12, 59, 87, 45],
+          }, {
+          label: 'Passagem',
+          backgroundColor: "#45c490",
+          data: [12, 59, 5, 56, 58,12, 59, 85, 23],
+          }, {
+          label: 'Medico',
+          backgroundColor: "#008d93",
+          data: [12, 59, 5, 56, 58,12, 59, 65, 51],
+          }, {
+          label: 'Outros',
+          backgroundColor: "#2e5468",
+          data: [12, 59, 5, 56, 58, 12, 59, 12, 74],
+          }]
+}
+
   return (
     <>
       <Nav/>
       <Container>
         <Content>
-          {nav === 1 &&
+          {nav === 2 &&
             <>
               <Form ref={formRef} onSubmit={handleSubmit}>
                 <Button type="submit">Buscar</Button>
@@ -137,6 +283,37 @@ export default function Dashboard() {
               <Bar type={"bar"} data={data} options={title}/>
             </>
           }
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <Button type="submit">Buscar</Button>
+            <Select click={onSelect}>
+              <div className="selectBox" onClick={() => setOnSelect(!onSelect)}>
+                <select>
+                    <option>Selecione as categorias</option>
+                </select>
+                <div id="checkBoxes">
+                    <label>
+                        <input type="checkbox" id="first" value="1"/>
+                        Pao
+                    </label>
+                    <label>
+                        <input type="checkbox" id="second" value="2"/>
+                        Leite
+                    </label>
+                </div>
+                <div className="overSelect"></div>
+              </div>
+              </Select>
+            <Input type="date" name="finalDate" placeholder="Data final"/>
+            <Input type="date" name="initialDate" placeholder="Data inicial"/>
+          </Form>
+          <Bar type={"Pie"}
+               data={data2}
+               options={options}
+               />
+            {/* <Bar type={"Pie"}
+          data={data2}
+          options={options}
+          /> */}
         </Content>
       </Container>
     </>
--- frontend/src/pages/Dashboard/styles.js
+++ frontend/src/pages/Dashboard/styles.js
@@ -17,3 +17,70 @@ export const Content = styled.div`
   background: #fff;
   margin-left: 20%;
 `;
+
+export const Select = styled.div`
+    width: 20%;
+    float: right;
+    margin-right:2%;
+    margin-top: 30px;
+
+  .selectBox {
+      position: relative;
+  }
+
+  .selectBox select {
+      width: 100%;
+      font-weight: bold;
+  }
+
+  .overSelect {
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+  }
+
+  #checkBoxes {
+     ${props => props.click ? 'display: block' : 'display: none'}
+      position: absolute;
+      width: 100%;
+      height: 100%;
+
+      font-size: 16px;
+      border: none;
+      background: #314295;
+      border: 0;
+      border-radius: 4px;
+      padding: 0 15px;
+      color: #fff;
+      margin: 0 0 10px;
+  }
+
+  #checkBoxes label {
+      display: block;
+  }
+/*
+  #checkBoxes label:hover {
+      background-color: #314295;
+  } */
+
+  span {
+    color: #314295;
+    font-size: 16px;
+  }
+
+  select {
+    width: 100%;
+    background: #314295;
+    border: 0;
+    border-radius: 4px;
+    height: 44px;
+    padding: 0 15px;
+    color: #fff;
+    margin: 0 0 10px;
+    &::placeholder {
+      color: rgba(255, 255, 255, 0.7);
+    }
+  }
+`;
